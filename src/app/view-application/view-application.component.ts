3ea627845ea54c3526007d5c3a608a43e7276ca8
Aa20475 <aa20475@gmail.com>
2021-07-22 22:13:46 +0530

    Added dynamic data loading in View application component

--- src/app/view-application/view-application.component.ts
+++ src/app/view-application/view-application.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-application',
   templateUrl: './view-application.component.html',
   styleUrls: ['./view-application.component.css']
 })
-export class ViewApplicationComponent implements OnInit {
+export class ViewApplicationComponent implements OnInit{
 
-  constructor() { }
+  applications:any;
+  url:string;
 
-  ngOnInit(): void {
+  constructor(private http:HttpClient) {
+    this.url = "http://localhost:8080";
+    
+    let promise = new Promise<void>( (resolve,request)=>{
+      this.http.get(this.url+"/application/all").toPromise().then(
+        res => {
+          resolve();
+          this.applications = res;
+        }
+      )
+    })
   }
 
+  ngOnInit(): void {
+
+  }
+
+  printdata():void{
+    console.log(this.applications);
+  }
 }
